0df2a6fa46c1c2a6d1d8d34d2adbcef1e6c62cb7
GeetanshMalik <geetanshmalik337@gmail.com>
2024-12-10 21:52:37 +0530

    Update spamDetector.py

--- spamDetector.py
+++ spamDetector.py
@@ -5,20 +5,20 @@ model = pickle.load(open('spam.pkl','rb'))
 cv = pickle.load(open('vectorizer.pkl','rb'))
 
 def main():
-	st.title("Email Spam Classification Application")
+	st.title("Email Spam Classification")
 	st.write("This is a Machine Learning application to classify emails as spam or ham.")
 	st.subheader("Classification")
-	user_input=st.text_area("Enter an email to classify" ,height=150)
-	if st.button("Classify"):
+	user_input=st.text_area("Enter an email to check whether it is spam or ham" ,height=150)
+	if st.button("Check"):
 		if user_input:
 			data=[user_input]
 			print(data)
 			vec=cv.transform(data).toarray()
 			result=model.predict(vec)
 			if result[0]==0:
-				st.success("This is Not A Spam Email")
+				st.success("HAM, This is NOT A Spam Email")
 			else:
-				st.error("This is A Spam Email")
+				st.error("SPAM, This is A Spam Email")
 		else:
 			st.write("Please enter an email to classify.")
 main()
